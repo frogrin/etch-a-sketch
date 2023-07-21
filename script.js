ab1ff6757537c65164891ed32e24df6b1fd09e6d
frog <136676079+frogrin@users.noreply.github.com>
2023-07-21 18:20:34 +0300

    Give hovered squares a background with randomized color

--- script.js
+++ script.js
@@ -1,5 +1,32 @@
 const container = document.querySelector('.container');
 const button = document.querySelector('.btn');
+
+function getRandomColor() {
+  let random = function () { return Math.floor(Math.random() * 256) };
+  let randomColor = `rgb(${random()},${random()},${random()})`;
+  return randomColor;
+}
+console.log(getRandomColor());
+function drawSquare(squaresPerSide) {
+  let div = document.createElement('div');
+  let squareDimension = `${960 / squaresPerSide}px`;
+  div.classList.add('square');
+  div.style.width = squareDimension;
+  div.style.height = squareDimension;
+  div.addEventListener('mouseover', () => div.style.backgroundColor = getRandomColor());
+  container.appendChild(div);
+}
+
+function drawGrid(n) {
+  for (let i = 0; i < n ** 2 ; i++) {
+    drawSquare(n);
+  } 
+}
+
+function removeSquares() {
+  container.innerHTML = '';
+}
+
 button.addEventListener('click', () => {
   let userSquares = +prompt('Squares per side(Max: 100)');
   if (userSquares > 100) {
@@ -9,25 +36,5 @@ button.addEventListener('click', () => {
     drawGrid(userSquares);
   }
 });
-
-function drawSquare(squaresPerSide) {
-  let div = document.createElement('div');
-  let squareDimension = `${960 / squaresPerSide}px`;
-  div.classList.add('square');
-  div.style.width = squareDimension;
-  div.style.height = squareDimension;
-  div.addEventListener('mouseover', e => e.target.classList = 'hovered');
-  container.appendChild(div);
-}
-
-function drawGrid(n) {
-  for (let i = 0; i < n * n; i++) {
-    drawSquare(n);
-  } 
-}
-
-function removeSquares() {
-  container.innerHTML = '';
-}
 
 drawGrid(46);
